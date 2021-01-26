8973c39fb8b1ebcae70b329d8f8facd3cbb37c56
KLZX121 <kenneth121lzx@gmail.com>
2021-01-27 09:16:56 +1000

    showed window when webContentst stopped loading

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ const { app, BrowserWindow, globalShortcut } = require('electron');
 
 function bootWindow(){
     const win = new BrowserWindow({
+        show: false
     });
+
     win.loadURL('https://qasmt.eq.daymap.net/daymap/student/dayplan.aspx');
+    win.webContents.on('did-stop-loading', () => win.maximize());
+
     globalShortcut.register('Ctrl+\\', ()=>{
         win.isVisible() ? win.hide() : win.show();
     });
