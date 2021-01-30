ba5c4349d03030d2c4c1f2ac62b4a9db520eb20b
KLZX121 <kenneth121lzx@gmail.com>
2021-01-30 10:55:35 +1000

    added credential login to proxy

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-const { app, BrowserWindow, globalShortcut } = require('electron');
+const { app, BrowserWindow, globalShortcut, ipcMain } = require('electron');
+const fs = require('fs');
+
+let credentials = getCreds();
+let loggedIn = false;
 
 function bootWindow(){
     const win = new BrowserWindow({
@@ -14,3 +18,35 @@ function bootWindow(){
 };
 
 app.once('ready', bootWindow);
+
+app.on('login', (event, webContents, details, authInfo, callback) => {
+    ipcMain.on('auth', () => {
+        credentials = getCreds();
+        callback(credentials.username, credentials.password);
+        loggedIn = true;
+    });
+    event.preventDefault();
+    if (!credentials.username || !credentials.password){
+        bootCredentials();
+    };
+    if (!loggedIn){
+        callback(credentials.username, credentials.password);
+        loggedIn = true;
+    } else {
+        bootCredentials('fail');
+        loggedIn = false;
+    };
+});
+function bootCredentials(fail){
+    const credWin = new BrowserWindow({
+        width: 400,
+        height: 210,
+        frame: false,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    credWin.loadFile('./credWin.html');
+    if (fail) credWin.webContents.send('fail', true);
+};
+function getCreds() {return JSON.parse(fs.readFileSync('./credentials.json'));};
